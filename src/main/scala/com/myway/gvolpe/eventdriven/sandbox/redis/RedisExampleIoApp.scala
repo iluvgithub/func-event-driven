470cba5f1d0c52fc4e8b53251d98ff2fa4357a55
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-07-21 00:59:40 +0200

    added redis

--- src/main/scala/com/myway/gvolpe/eventdriven/sandbox/redis/RedisExampleIoApp.scala
+++ src/main/scala/com/myway/gvolpe/eventdriven/sandbox/redis/RedisExampleIoApp.scala
@@ -13,7 +13,7 @@ object RedisExampleIoApp extends IOApp.Simple:
   // sudo systemctl start redis-server
   // ps aux | grep redis-server
   val redisConnection = "redis://localhost"
-  val key = "language"
+  val key             = "language"
 
   val resources: Resource[IO, RedisCommands[IO, String, String]] =
     for
@@ -26,9 +26,9 @@ object RedisExampleIoApp extends IOApp.Simple:
 
   def run: IO[Unit] = resources.use(setGet)
 
-  def setGet[F[_] : Monad : Console](redis: RedisCommands[F, String, String]): F[Unit] =
+  def setGet[F[_]: Monad: Console](redis: RedisCommands[F, String, String]): F[Unit] =
     for
-      _ <- redis.set(key, "Scala3")
+      _        <- redis.set(key, "Scala3")
       optValue <- redis.get(key)
       value = optValue.getOrElse(s"not found key:${key}")
       _ <- Console[F].println(s"Read from Redis: $value")
